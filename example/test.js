ebf509aa87d941e2662a61c1fefe8ac9f2420fb9
Michael Gummere <me@disilla.org>
2022-09-29 11:40:03 -0400

    remove example for splitout

--- example/test.js
+++
@@ -1,7 +0,0 @@
-const { Katana } = require("../src/index");
-const blade = new Katana("./db", { encrypt: true, saveToDisk: true });
-
-blade.purgeState();
-for (let index = 0; index < 1; index++) {
-	blade.push("Hello!", `${Math.random() * 10}`);
-}
