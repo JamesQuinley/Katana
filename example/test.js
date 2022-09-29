c18558f80b2320374253496a9a4158a74a440ce0
Michael Gummere <me@disilla.org>
2022-09-29 11:29:30 -0400

    seperate class from module index

--- example/test.js
+++ example/test.js
@@ -1,6 +1,7 @@
-const Katana = require("../src/index");
-const blade = new Katana("./db", { encrypt: true });
+const { Katana } = require("../src/index");
+const blade = new Katana("./db", { encrypt: true, saveToDisk: true });
 
-// blade.purgeState();
-// blade.push("Hello World!", "greeting");
-console.log(blade.get("greeting"));
+blade.purgeState();
+for (let index = 0; index < 1; index++) {
+	blade.push("Hello!", `${Math.random() * 10}`);
+}
